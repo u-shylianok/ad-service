821fb3781bd8c9df0ca0c4d99cab9530e65adea9
u-shylianok <u.shylianok@gmail.com>
2021-11-12 15:00:59 +0300

    Remove unused funcs

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -4,19 +4,3 @@ type Tag struct {
 	ID   int    `db:"id"`
 	Name string `db:"name"`
 }
-
-func TagsToListIDs(tags []Tag) []int {
-	result := make([]int, len(tags))
-	for i, tag := range tags {
-		result[i] = tag.ID
-	}
-	return result
-}
-
-func TagsToListNames(tags []Tag) []string {
-	result := make([]string, len(tags))
-	for i, tag := range tags {
-		result[i] = tag.Name
-	}
-	return result
-}
