0cb822865a847b669adb2365902a1396b64de9cb
u-shylianok <u.shylianok@gmail.com>
2021-11-18 12:02:06 +0300

    Add ListPhotos test and fix parallel mode

--- internal/service/photo_test.go
+++ internal/service/photo_test.go
@@ -9,17 +9,16 @@ import (
 )
 
 func TestPhotoService_ListAdPhotos(t *testing.T) {
-	for scenario, fn := range map[string]func(t *testing.T){
+	cases := map[string]func(t *testing.T){
 
 		"valid result": func(t *testing.T) {
 
 			expectedPhotos := []string{"photo 1", "photo 2"}
-			expectedError := error(nil)
 
 			repo := repository.PhotoMock{}
 			repo.ListLinksByAdReturns(
 				expectedPhotos,
-				expectedError,
+				nil,
 			)
 
 			ps := &PhotoService{
@@ -48,9 +47,63 @@ func TestPhotoService_ListAdPhotos(t *testing.T) {
 			require.Error(t, err)
 			require.EqualError(t, err, expectedError.Error())
 		},
-	} {
+	}
+
+	for scenario, fn := range cases {
+		testFunc := fn
 		t.Run(scenario, func(t *testing.T) {
-			fn(t)
+			t.Parallel()
+			testFunc(t)
+		})
+	}
+}
+
+func TestPhotoService_ListPhotos(t *testing.T) {
+	cases := map[string]func(t *testing.T){
+
+		"valid result": func(t *testing.T) {
+
+			expectedPhotos := []string{"photo 1", "photo 2", "photo 3", "photo 4"}
+
+			repo := repository.PhotoMock{}
+			repo.ListLinksReturns(
+				expectedPhotos,
+				nil,
+			)
+
+			ps := &PhotoService{
+				photoRepo: &repo,
+			}
+
+			photos, err := ps.ListPhotos()
+			require.NoError(t, err)
+			require.EqualValues(t, expectedPhotos, photos)
+		},
+		"error result": func(t *testing.T) {
+
+			expectedError := fmt.Errorf("some error")
+
+			repo := repository.PhotoMock{}
+			repo.ListLinksReturns(
+				nil,
+				expectedError,
+			)
+
+			ps := &PhotoService{
+				photoRepo: &repo,
+			}
+
+			_, err := ps.ListPhotos()
+			require.Error(t, err)
+			require.EqualError(t, err, expectedError.Error())
+		},
+	}
+
+	for scenario, fn := range cases {
+		testFunc := fn
+		t.Run(scenario, func(t *testing.T) {
+			t.Parallel()
+			testFunc(t)
 		})
 	}
 }
