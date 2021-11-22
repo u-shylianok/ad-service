8318ad23ec9474f6ac93061294d9217fa1193c40
u-shylianok <u.shylianok@gmail.com>
2021-11-22 17:19:55 +0300

    Change photo tests to pattern

--- internal/service/photo_test.go
+++ internal/service/photo_test.go
@@ -9,101 +9,117 @@ import (
 )
 
 func TestPhotoService_ListAdPhotos(t *testing.T) {
-	cases := map[string]func(t *testing.T){
+	type fields struct {
+		photoRepo *repository.PhotoMock
+	}
+	type args struct {
+		adID int
+	}
+	tests := []struct {
+		name   string
+		setup  func(*fields)
+		args   args
+		assert func(*testing.T, *fields, []string, error)
+	}{
+		{
+			name: "success - without errors",
+			setup: func(f *fields) {
+				repo := &repository.PhotoMock{}
+				repo.ListLinksByAdReturns([]string{"photo 1", "photo 2"}, nil)
 
-		"valid result": func(t *testing.T) {
-
-			expectedPhotos := []string{"photo 1", "photo 2"}
-
-			repo := repository.PhotoMock{}
-			repo.ListLinksByAdReturns(
-				expectedPhotos,
-				nil,
-			)
-
-			ps := &PhotoService{
-				photoRepo: &repo,
-			}
-
-			photos, err := ps.ListAdPhotos(1)
-			require.NoError(t, err)
-			require.EqualValues(t, expectedPhotos, photos)
+				f.photoRepo = repo
+			},
+			args: args{
+				adID: 1,
+			},
+			assert: func(t *testing.T, f *fields, photos []string, err error) {
+				require.NoError(t, err)
+				require.Equal(t, []string{"photo 1", "photo 2"}, photos)
+			},
 		},
-		"error result": func(t *testing.T) {
+		{
+			name: "fail - some errors occurred",
+			setup: func(f *fields) {
+				repo := &repository.PhotoMock{}
+				repo.ListLinksByAdReturns(nil, fmt.Errorf("some error"))
 
-			expectedError := fmt.Errorf("some error")
-
-			repo := repository.PhotoMock{}
-			repo.ListLinksByAdReturns(
-				nil,
-				expectedError,
-			)
-
-			ps := &PhotoService{
-				photoRepo: &repo,
-			}
-
-			_, err := ps.ListAdPhotos(1)
-			require.Error(t, err)
-			require.EqualError(t, err, expectedError.Error())
+				f.photoRepo = repo
+			},
+			args: args{
+				adID: 1,
+			},
+			assert: func(t *testing.T, f *fields, photos []string, err error) {
+				require.Error(t, err)
+				require.EqualError(t, err, "some error")
+				require.Equal(t, []string(nil), photos)
+			},
 		},
 	}
-
-	for scenario, fn := range cases {
-		testFunc := fn
-		t.Run(scenario, func(t *testing.T) {
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
-			testFunc(t)
+
+			var fields fields
+			test.setup(&fields)
+
+			photoService := NewPhotoService(fields.photoRepo)
+			photos, err := photoService.ListAdPhotos(test.args.adID)
+
+			test.assert(t, &fields, photos, err)
 		})
 	}
 }
 
 func TestPhotoService_ListPhotos(t *testing.T) {
-	cases := map[string]func(t *testing.T){
+	type fields struct {
+		photoRepo *repository.PhotoMock
+	}
+	tests := []struct {
+		name   string
+		setup  func(*fields)
+		assert func(*testing.T, *fields, []string, error)
+	}{
+		{
+			name: "success - without errors",
+			setup: func(f *fields) {
+				repo := &repository.PhotoMock{}
+				repo.ListLinksReturns([]string{"photo 1", "photo 2", "photo 3", "photo 4"}, nil)
 
-		"valid result": func(t *testing.T) {
-
-			expectedPhotos := []string{"photo 1", "photo 2", "photo 3", "photo 4"}
-
-			repo := repository.PhotoMock{}
-			repo.ListLinksReturns(
-				expectedPhotos,
-				nil,
-			)
-
-			ps := &PhotoService{
-				photoRepo: &repo,
-			}
-
-			photos, err := ps.ListPhotos()
-			require.NoError(t, err)
-			require.EqualValues(t, expectedPhotos, photos)
+				f.photoRepo = repo
+			},
+			assert: func(t *testing.T, f *fields, photos []string, err error) {
+				require.NoError(t, err)
+				require.Equal(t, []string{"photo 1", "photo 2", "photo 3", "photo 4"}, photos)
+			},
 		},
-		"error result": func(t *testing.T) {
+		{
+			name: "fail - some errors occurred",
+			setup: func(f *fields) {
+				repo := &repository.PhotoMock{}
+				repo.ListLinksReturns(nil, fmt.Errorf("some error"))
 
-			expectedError := fmt.Errorf("some error")
-
-			repo := repository.PhotoMock{}
-			repo.ListLinksReturns(
-				nil,
-				expectedError,
-			)
-
-			ps := &PhotoService{
-				photoRepo: &repo,
-			}
-
-			_, err := ps.ListPhotos()
-			require.Error(t, err)
-			require.EqualError(t, err, expectedError.Error())
+				f.photoRepo = repo
+			},
+			assert: func(t *testing.T, f *fields, photos []string, err error) {
+				require.Error(t, err)
+				require.EqualError(t, err, "some error")
+				require.Equal(t, []string(nil), photos)
+			},
 		},
 	}
-
-	for scenario, fn := range cases {
-		testFunc := fn
-		t.Run(scenario, func(t *testing.T) {
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
-			testFunc(t)
+
+			var fields fields
+			test.setup(&fields)
+
+			photoService := NewPhotoService(fields.photoRepo)
+			photos, err := photoService.ListPhotos()
+
+			test.assert(t, &fields, photos, err)
 		})
 	}
 }
